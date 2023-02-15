f6a81d0862b7598715bf00214fbad8ed4d32fec6
PattenChiou <patten1719@gmail.com>
2023-02-15 13:15:21 +0800

    show total price

--- scripts/purchase.js
+++ scripts/purchase.js
@@ -1,9 +1,28 @@
 const scriptURL="https://script.google.com/macros/s/AKfycbxRGlLG5yd0ZkvdskDlOHTEeje95PE0BoZavjuf_J1dZjFoFfrAVq_YZsVkDTArixSp/exec"
 const form = document.forms['purchase']
+$("#submit").click(function (e){
+    e.preventDefault();
+    fetch(scriptURL,{method: "POST",body: new FormData(form)})
+    .then(response => alert("You have successfully submitted."))
+    .catch(error => console.error("Error!", error.message))
+  }
+)
+
+/*const form = document.forms['purchase']
           
           form.addEventListener('submit', e => {
             e.preventDefault()
             fetch(scriptURL, { method: 'POST', body: new FormData(form)})
               .then(response => alert("You have successfully submitted."))
               .catch(error => console.error('Error!', error.message))
-          })
+          })*/
+
+let price = {"肉片":200};
+input_total = $("#total");
+quantity = $("#quantity");
+product_name = $("#product_name");
+function setTotal(){
+  input_total.val(String(quantity.val()*price[product_name.val()]));
+  //("總價:" + String(quantity.val()*price[product_name.val()]))
+}
+setInterval(setTotal,100);
